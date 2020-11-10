0400d7a1db595bfed5fe4da950f0938349f2e77b
Joel Holmes <holmes89@gmail.com>
2020-11-09 20:28:56 -0500

    timer

--- apps/feed_watcher/lib/feed_watcher.ex
+++ apps/feed_watcher/lib/feed_watcher.ex
@@ -1,21 +1,43 @@
 defmodule FeedWatcher do
+  use GenServer
   @moduledoc """
   Documentation for `FeedWatcher`.
   """
 
   @doc """
-  Hello world.
+  Fetch data from endpoint.
 
   ## Examples
 
-      iex> FeedWatcher.hello()
-      :world
+      iex> FeedWatcher.fetch(endpoint)
+      fetchs data from endpoint
 
   """
   def fetch(endpoint) do
+
+  end
+
+  def init(endpoint) do
     HTTPoison.start
+    send(self(), :poll) # Immediately schedule the first poll
+    {:ok, endpoint}
+  end
+
+  def handle_info(:fetch, endpoint) do
     {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(endpoint)
     {:ok, feed, _} = FeederEx.parse(body)
-    Enum.map feed.entries, fn(entry) -> %{id: (:crypto.hash(:sha256, entry.id) |> Base.encode16()) , url: entry.link, updated: entry.updated} end
+    results = Enum.map feed.entries, fn(entry) -> %{id: (:crypto.hash(:sha256, entry.link) |> Base.encode16()) , url: entry.link, updated: entry.updated} end
+    # IO.inspect results
+    {:noreply, endpoint}
+  end
+
+  def handle_info(:poll, state) do
+
+    send(self(), :fetch) # Immediately schedule the first poll
+
+    # Re-schedule the next poll in 1 minute (60_000 ms):
+    Process.send_after(self, :poll, 60_000)
+
+    {:noreply, state}
   end
 end
